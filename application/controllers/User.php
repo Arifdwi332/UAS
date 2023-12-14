eef8bce4ca49b42be51ba5bde471be5f2327aed3
Arriipp MorningstaR <ariefdwi332@gmail.com>
2023-12-14 12:32:11 +0700

    pudh ketiga

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,8 +5,10 @@ class User extends CI_Controller
 {
     public function index()
     {
+        $data['title'] = 'Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' =>
         $this->session->userdata('email')])->row_array();
-        echo 'Selamat datang ' . $data['user']['name'];
+
+        $this->load->view('user/index', $data);
     }
 }
